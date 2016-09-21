2a2ff3e99ef0352894a2253afdabe795802d4f34
hartho3 <t.hardieck@web.de>
2016-09-21 12:33:30 +0200

    charts

--- test/js/statistik.js
+++ test/js/statistik.js
@@ -1,3 +1,4 @@
+Chart.defaults.global.defaultFontSize = 20;
 
 var totaldata= JSON.parse(document.getElementById("data").innerHTML);
 var spielTage=[];
@@ -9,7 +10,7 @@ var data = {
     labels: spielTage,
     datasets: [
         {
-            label: "Punkte:",
+            label: "Saison 1516",
             fill: false,
             lineTension: 0.1,
             backgroundColor: "#ffffff",
@@ -26,14 +27,109 @@ var data = {
             pointRadius: 1,
             pointHitRadius: 10,
             data: totaldata[0].test1516
+        },
+
+        {
+          label: "Saison 1415",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "#ffffff",
+          borderColor: "rgba(139, 0, 0,1)",
+          borderCapStyle: 'butt',
+          borderJoinStyle: 'miter',
+          pointBorderColor: "rgba(139, 0, 0,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(139, 0, 0,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+
+          data: totaldata[1].test1415
+        },
+        {
+          label: "Saison 1314",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "#ffffff",
+          borderColor: "rgba(255,255,0,1)",
+          borderCapStyle: 'butt',
+          borderJoinStyle: 'miter',
+          pointBorderColor: "rgba(255,255,0,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(255,255,0,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: totaldata[2].test1314
+        },
+        {
+          label: "Saison 1213",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "#ffffff",
+          borderColor: "rgba(255,0,255,1)",
+          borderCapStyle: 'butt',
+          borderJoinStyle: 'miter',
+          pointBorderColor: "rgba(255,0,255,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(255,0,255,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: totaldata[3].test1213
+        },
+        {
+          label: "Saison 1112",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "#ffffff",
+          borderColor: "rgba(128,0,255,1)",
+          borderCapStyle: 'butt',
+          borderJoinStyle: 'miter',
+          pointBorderColor: "rgba(128,0,255,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(128,0,255,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: totaldata[4].test1112
         }
+
+
     ]
 };
 var ctx = document.getElementById("LineChart");
 
 var myLineChart = new Chart(ctx, {
     type: 'line',
-    data: data
+    data: data,
+    defaultFontSize: 2100,
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    stepSize: 1,
+                    callback: function(value, index, values) {
+                      if(value === 0) return "N";
+                      if(value === 1) return "U";
+                      if(value === 2) return "S";
+                }
+              }
+            }]
+        }
+    }
 
 });
 
@@ -44,7 +140,7 @@ var numDraw =0;
 var numLost =0;
 
 for (var i=0; i<totaldata[0].test1516.length;i++){
-  if(totaldata[0].test1516[i]==3){
+  if(totaldata[0].test1516[i]==2){
     numWin++;
   }
   if(totaldata[0].test1516[i]==1){
